9c371da099e2c593409f6f32e7ed758751b0c00c
SpicyCoder <spk2naveen@hotmail.com>
2026-01-01 22:24:18 +0530

    Fix

--- DesignPatterns/Creational/Singleton.cs
+++ DesignPatterns/Creational/Singleton.cs
@@ -6,5 +6,5 @@ public class Singleton
 
     private static readonly Lazy<Singleton> _instance = new(() => new Singleton());
 
-    public Singleton Instance = _instance.Value;
+    public static Singleton Instance = _instance.Value;
 }
